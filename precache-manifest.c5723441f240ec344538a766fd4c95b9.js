4f8a1f23e0640643e086c7895264c2caadc53200
NightWing1998 <dhruvil.s@somaiya.edu>
2020-02-14 19:15:32 +0530

    about page prototype ready

---
+++ precache-manifest.c5723441f240ec344538a766fd4c95b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f575dc381cc6a0ae470f0d19a5605f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223e144020dddfa39db6",
+    "url": "/static/css/main.83947c40.chunk.css"
+  },
+  {
+    "revision": "0ac2038c596eb184ffeb",
+    "url": "/static/js/2.5233d75b.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/static/js/2.5233d75b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "223e144020dddfa39db6",
+    "url": "/static/js/main.417976ef.chunk.js"
+  },
+  {
+    "revision": "48e6ed1aa7d2e9247982",
+    "url": "/static/js/runtime-main.bc1e2fa9.js"
+  },
+  {
+    "revision": "3349601036b2d3b6aa9283bd7b50b956",
+    "url": "/static/media/bg.33496010.jpg"
+  },
+  {
+    "revision": "9aa189abdc4d691ba3b5fcae21774c9c",
+    "url": "/static/media/me.9aa189ab.jpg"
+  }
+]);
