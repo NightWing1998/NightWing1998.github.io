44d103c8d974fe0a8b9a2347d42e1b42440b2b27
NightWing1998 <dhruvil.s@somaiya.edu>
2020-03-30 03:15:56 +0530

    updating skills

--- ColorGame.js
+++ ColorGame.js
@@ -9,62 +9,68 @@ var tiles = 6;
 
 var colors = generateRandomColors(tiles);
 
-var ans = colors[Math.floor( Math.random()*colors.length )];
+var ans = colors[Math.floor(Math.random() * colors.length)];
 
 var coldisp = document.getElementById("huge");
 coldisp.textContent = ans;
 coldisp.style.textTransform = "uppercase";
 
 for (var i = tiles - 1; i >= 0; i--) {
-	sq[i].style.background = colors[i] ;
+	sq[i].style.background = colors[i];
 
-	sq[i].addEventListener( "click",function(){
-		if( ans === this.style.background )
-		{
+	sq[i].addEventListener("click", function (event) {
+		console.log(event.target);
+		if (ans === event.target.style.background) {
 			res.textContent = "Correct!!!";
-			document.querySelector(".heading").style.backgroundColor = this.style.background;
+			document.querySelector(".heading").style.backgroundColor = event.target.style.background;
 			for (var i = tiles - 1; i >= 0; i--) {
 				sq[i].style.background = ans;
 			}
 			reset.textContent = "Play Again?"
-		}
-		else
-		{
+		} else {
 			res.textContent = "Try Again";
 			this.style.background = "inherit";
 		}
-	} )
+	})
 }
 
-reset.addEventListener("click",resetFunc );
+reset.addEventListener("click", resetFunc);
 
-easy.addEventListener( "click" , function(){tiles = 3;this.classList.add("selected");hard.classList.remove("selected");resetFunc();} );
-hard.addEventListener( "click" , function(){tiles = 6;this.classList.add("selected");easy.classList.remove("selected");resetFunc();} );
+easy.addEventListener("click", function () {
+	tiles = 3;
+	this.classList.add("selected");
+	hard.classList.remove("selected");
+	resetFunc();
+});
+hard.addEventListener("click", function () {
+	tiles = 6;
+	this.classList.add("selected");
+	easy.classList.remove("selected");
+	resetFunc();
+});
 
-function generateRandomColors(num)
-{
+function generateRandomColors(num) {
 	var arr = [];
-	for (var i = num - 1; i >= 0; i--) 
-	{
-		arr.push( "rgb("+ (Math.floor( Math.random()*256 )) + ", " + (Math.floor( Math.random()*256 )) + ", " + (Math.floor( Math.random()*256 )) + ")" );
+	for (var i = num - 1; i >= 0; i--) {
+		arr.push("rgb(" + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ")");
 	}
 	return arr;
 }
 
-function resetFunc(){
+function resetFunc() {
 	colors = generateRandomColors(tiles);
 
-	ans = colors[Math.floor( Math.random()*colors.length )];
+	ans = colors[Math.floor(Math.random() * colors.length)];
 
 	coldisp.textContent = ans;
 	coldisp.style.textTransform = "uppercase";
 
 	for (var i = sq.length - 1; i >= 0; i--) {
-		sq[i].style.background = "inherit" ;
+		sq[i].style.background = "inherit";
 	}
 
 	for (var i = tiles - 1; i >= 0; i--) {
-		sq[i].style.background = colors[i] ;
+		sq[i].style.background = colors[i];
 	}
 	document.querySelector(".heading").style.backgroundColor = "steelblue";
 	res.textContent = "";
