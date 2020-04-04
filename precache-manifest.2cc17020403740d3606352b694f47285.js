a961ccba741d67a74e3374f0ea4dd4f568b9e7c3
NightWing1998 <dhruvil.s@somaiya.edu>
2020-04-04 18:13:30 +0530

    projects update heroku

---
+++ precache-manifest.2cc17020403740d3606352b694f47285.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb3e37eb715285924a5bb960c973de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f1d881acd76518a7ae0",
+    "url": "/static/css/2.cbe2632d.chunk.css"
+  },
+  {
+    "revision": "8ad83accc6f8d7401623",
+    "url": "/static/css/main.0fa44aa0.chunk.css"
+  },
+  {
+    "revision": "8f1d881acd76518a7ae0",
+    "url": "/static/js/2.feb25a53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.feb25a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad83accc6f8d7401623",
+    "url": "/static/js/main.4707c85e.chunk.js"
+  },
+  {
+    "revision": "556059dfffe46f8d618e",
+    "url": "/static/js/runtime-main.1a86658a.js"
+  },
+  {
+    "revision": "650a7b8ffaab9a1d71c776e45654b366",
+    "url": "/static/media/biscuit.650a7b8f.svg"
+  },
+  {
+    "revision": "7e4eec1cbf83b3dd8ae9df79035b2a0c",
+    "url": "/static/media/emfinity.7e4eec1c.png"
+  },
+  {
+    "revision": "8c37aa9287af7562318690f3ec435727",
+    "url": "/static/media/law.8c37aa92.svg"
+  },
+  {
+    "revision": "de0699e8ccf98c24b9202665e2f665eb",
+    "url": "/static/media/rice.de0699e8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c4594b64acf6205119ed72ffaac65f2.js"
+  "/precache-manifest.2cc17020403740d3606352b694f47285.js"
 );
 
 self.addEventListener('message', (event) => {
